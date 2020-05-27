4b441f54217bbb7ee30bfb9e78fba8a7ecae70ca
Savitha Gollamudi <savithagollamudi@github.com>
2020-05-27 19:52:18 +0530

    updated url in config file

--- notes-website/docusaurus.config.js
+++ notes-website/docusaurus.config.js
@@ -88,12 +88,14 @@ module.exports = {
           homePageId: "doc1",
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl: "https://github.com/savithagollamudi/notebook/",
+          editUrl:
+            "https://github.com/savithagollamudi/notebook/tree/master/notes-website",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl: "https://github.com/savithagollamudi/notebook/blog/",
+          editUrl:
+            "https://github.com/savithagollamudi/notebook/tree/master/notes-website/blog/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
